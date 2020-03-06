cb79d1bd0e4c1d958f497de70145c99cffd11fd8
Anna <anna.gillespie@ucsf.edu>
2020-03-05 22:34:23 -0800

    all AKG notebook update

--- Dec_1d_v2_virgas_AKG.py
+++ Dec_1d_v2_virgas_AKG.py
@@ -123,7 +123,7 @@ def main(path_base, rat_name, day, epoch, shift_amt, path_out, velthresh=4, use_
     binned_linear_pos, binned_arm_coords, pos_bins = sungod_util.bin_position_data(linear_pos_raw, arm_coords, pos_bin_size)
 
     # important for new arm nodes:
-    binned_arm_coords[:,1] = 1+binned_arm_coords[:,1]
+    #binned_arm_coords[:,1] = 1+binned_arm_coords[:,1]
 
     # calculate bin coverage based on determined binned arm bounds   TO DO: prevent the annnoying "copy of a slice" error [prob need .values rather than a whole column]
     #pos_bin_delta = sungod_util.define_pos_bin_delta(binned_arm_coords, pos_bins, linear_pos_raw, pos_bin_size)
